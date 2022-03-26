ee80f98384c7271cdf4af162a5cbd8a87764200d
Uli-mgh <ulicmgh@gmail.com>
2022-03-26 18:24:23 -0300

    Backend

---
+++ backend/src/config.js
@@ -0,0 +1,14 @@
+const { config } = require("dotenv");
+
+config();
+console.log(process.env.HELLO);
+
+module.exports = {
+  db: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+  },
+};
